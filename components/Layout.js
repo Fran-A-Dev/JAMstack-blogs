256ec382942afe405dbae2e29b3e222b1d5324df
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2021-12-18 12:57:56 -0600

    updated scaffold

---
+++ components/Layout.js
@@ -0,0 +1,29 @@
+
+import Link from 'next/link'
+
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <header>
+        <Link href="/">
+          <a>
+            <h1>
+              <span>Fran_The_Dev</span>
+              <span>JAMstack Blogs</span>
+            </h1>
+            <h2>JAMstack Joy</h2>
+          </a>
+        </Link>
+      </header>
+
+      <div className="page-content">
+        { children }
+      </div>
+
+      <footer>
+        <p>Copyright 2021 Fran_The_Dev</p>
+      </footer>
+    </div>
+  )
+}
+
