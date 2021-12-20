17d1c87e46cf94bdded10c88b756d2052dab9631
Francis Agulto <francisagulto@franciss-mbp.lan>
2021-12-19 21:22:22 -0800

    added environment

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
-export default function Recipes() {
-  return (
+import { createClient } from "contentful"
+
+
+export async function getStaticProps() {
+
+
+  const client = createClient({ 
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY, 
+  })
+
+const res = await client.getEntries({ content_type:'memory'})
+
+return {
+  props: {
+    memories: res.items
+  }
+}
+
+} 
+
+export default function Home({memories}) {
+  console.log(memories)
+return (
     <div className="blog-list">
       Nerd Blogs
     </div>
   )
 }
+
+
