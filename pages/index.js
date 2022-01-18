cce8108e9486607a2a7f5dc3c07ed5be07dbb4b4
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2022-01-18 11:31:19 -0600

    re-styled in tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,28 @@
-import { createClient } from 'contentful'
-import BlogCard from '../components/BlogCard'
+import { createClient } from "contentful";
+import BlogList from "../components/BlogList";
 
 export async function getStaticProps() {
-
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY,
-  })
+  });
 
-  const res = await client.getEntries({ content_type: "memory" })
+  const res = await client.getEntries({ content_type: "memory" });
 
   return {
     props: {
       blogs: res.items,
-      revalidate: 1
-    }
-  }
+      revalidate: 1,
+    },
+  };
 }
 
 export default function Recipes({ blogs }) {
-  console.log(blogs)
+  console.log(blogs);
 
   return (
-    <div className="blog-list">
-   {blogs.map(blog => (
-     <BlogCard key={blog.sys.id} blog={blog}    />
-   ))}
-
-
-   <style jsx>{`
-   
-   .blog-list {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 20px 60px;
-  }
-   
-   `}</style>
-       </div>
-  )
+    <div className="text-3xl">
+      <BlogList blogs={blogs} />
+    </div>
+  );
 }
-
-
